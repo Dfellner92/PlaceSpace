8102d85d0be96f03c0da6306c3250b03b4e69037
Dan Fellner <dfellner92@gmail.com>
2020-11-29 00:26:29 -0500

    finished modal and modal css

--- Frontend/src/shared/components/UIElements/Modal.js
+++ Frontend/src/shared/components/UIElements/Modal.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
 
+import Backdrop from './Backdrop';
 import './Modal.css';
 
 const ModalOverlay = props => {
@@ -26,7 +29,18 @@ const ModalOverlay = props => {
 };
 
 const Modal = props => {
-
+    return <React.Fragment>
+        {props.show && <Backdrop onClick={props.onCancel} />}
+        <CSSTransition
+            in={props.show}
+            mountOnEnter
+            unmountOnExit
+            timeout={200}
+            classNames="modal"
+        >
+            <ModalOverlay {...props} />
+        </CSSTransition>
+    </React.Fragment>
 };
 
 export default Modal;
